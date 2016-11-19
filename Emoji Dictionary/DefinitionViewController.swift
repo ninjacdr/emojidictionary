1d34a563b4e4bb856c07ffc3b6402f0947c92c47
J <weezie@weeZies-Mac-mini.local>
2016-11-18 19:15:34 -0600

    Finished emoji dictionary

---
+++ Emoji Dictionary/DefinitionViewController.swift
@@ -0,0 +1,50 @@
+//
+//  DefinitionViewController.swift
+//  Emoji Dictionary
+//
+//  Created by weeZie on 11/18/16.
+//  Copyright © 2016 weeZie. All rights reserved.
+//
+
+import UIKit
+
+class DefinitionViewController: UIViewController {
+
+    @IBOutlet weak var definitionLabel: UILabel!
+    @IBOutlet weak var emojiLabel: UILabel!
+    
+    var emoji = "No Emoji"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        emojiLabel.text = emoji
+        
+        if emoji == "🐶" {
+        definitionLabel.text = "A cute puppy!"
+        }
+        if emoji == "😎" {
+            definitionLabel.text = "It's a sunny happy day!"
+        }
+        if emoji == "✌️" {
+            definitionLabel.text = "Peace out!"
+        }
+        if emoji == "🎃" {
+            definitionLabel.text = "Happy Halloween!"
+        }
+        if emoji == "🎨" {
+            definitionLabel.text = "Artistic"
+        }
+        if emoji == "👀" {
+            definitionLabel.text = "What are those!"
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
